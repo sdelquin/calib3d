6abfcc15b68b39922717929746b8087398c8f24f
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-12-22 15:54:37 +0000

    Display table of results

--- calib3d/core.py
+++ calib3d/core.py
@@ -15,25 +15,51 @@ class Calibration:
     def read_measurements(self):
         self.measurements = {}
         print(
-            f'[dim]Enter measurements of {self.config["cube-side"]}mm '
+            f'[dim]Enter measurements for {self.config["cube-side"]}mm '
             'calibration cube:[/dim]'
         )
         for axis, color in settings.AXIS_COLORS.items():
             print(f'[bold {color}]{axis}:[/bold {color}]', end=' ')
             self.measurements[axis] = float(input())
 
+    def calculate_errors(self):
+        self.errors = {}
+        for axis, measurement in self.measurements.items():
+            self.errors[axis] = measurement - self.config['cube-side']
+
     def fix_steps(self):
         self.steps = {}
         for axis, measurement in self.measurements.items():
             self.steps[axis] = (
-                measurement * self.config['steps'][axis] / self.config['cube-side']
+                measurement * self.config['axis-steps'][axis] / self.config['cube-side']
             )
 
     def show_steps(self):
-        table = Table(show_header=True, header_style="bold magenta")
-        for axis in self.steps.keys():
-            table.add_column(f'{axis} steps')
+        table = Table()
+        table.add_column(style='italic')
+
+        for axis, color in settings.AXIS_COLORS.items():
+            table.add_column(f'{axis}', header_style=color)
+
+        values = (f'{measurement:.2f}' for measurement in self.measurements.values())
+        table.add_row('Measurements', *values)
+
+        display = []
+        for error in self.errors.values():
+            if error > 0:
+                color = 'green'
+            elif error < 0:
+                color = 'red'
+            else:
+                color = 'white'
+            display.append(f'[{color}]{error:.2f}[/{color}]')
+        table.add_row('Errors', *display)
+
+        values = (f'{steps:.2f}' for steps in self.config['axis-steps'].values())
+        table.add_row('Current steps', *values)
+
         values = (f'{steps:.2f}' for steps in self.steps.values())
-        table.add_row(*values)
+        table.add_row('Fixed steps', *values, style='yellow')
+
         console = Console()
         console.print(table)
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from calib3d import Calibration
 
 calib = Calibration()
 calib.read_measurements()
+calib.calculate_errors()
 calib.fix_steps()
 calib.show_steps()
