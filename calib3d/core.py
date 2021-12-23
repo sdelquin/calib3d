c3a3b9ee8b486fd8a1aebd38f344317b3d72a4cc
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-12-23 12:35:16 +0000

    Refactor display

--- calib3d/core.py
+++ calib3d/core.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 
 import yaml
-from rich.console import Console
-from rich.table import Table
 
 import settings
-from calib3d import utils
+from calib3d import display
 
 
 class Calibration:
@@ -45,42 +43,4 @@ class Calibration:
         self.config_file.write_text(output)
 
     def show_results(self, show_gcode=False):
-        console = Console()
-
-        table = Table()
-        table.add_column(style='italic')
-
-        for axis, color in settings.AXIS_COLORS.items():
-            table.add_column(f'{axis}', header_style=color)
-
-        values = (f'{steps:.2f}' for steps in self.config['steps'].values())
-        table.add_row('Current steps', *values, end_section=True)
-
-        values = (f'{measurement:.2f}' for measurement in self.measurements.values())
-        table.add_row('Measurements', *values)
-
-        valid_error = self.config['calcube']['valid-error']
-        values = (
-            f'{utils.format_value(error, threshold=valid_error)}'
-            for error in self.errors.values()
-        )
-        table.add_row('Errors', *values, end_section=True)
-
-        values = (f'{steps:.2f}' for steps in self.steps.values())
-        table.add_row('Fixed steps', *values, style='yellow')
-
-        console.print(table)
-
-        console.print(
-            'Calibrating 3D printer stepping motors from a '
-            f'{self.config["calcube"]["side"]}mm cube',
-            style='dim italic',
-        )
-
-        if show_gcode:
-            fixed_steps = ' '.join(
-                f'{axis}{step:.2f}'
-                for axis, step in self.steps.items()
-                if not self.valid_errors[axis]
-            )
-            console.print(f'[dark_orange]G-code: M92 {fixed_steps}[/dark_orange]')
+        display.Display(self, show_gcode).print()
---
+++ calib3d/display.py
@@ -0,0 +1,75 @@
+from rich.console import Console
+from rich.table import Table
+
+import settings
+from calib3d import utils
+
+
+class Display:
+    def __init__(self, calibration, show_gcode=False):
+        self.calibration = calibration
+        self.show_gcode = show_gcode
+        self.console = Console()
+
+    def add_header(self, table):
+        table.add_column(style='italic')
+        for axis, color in settings.AXIS_COLORS.items():
+            table.add_column(f'{axis}', header_style=color)
+
+    def add_current_steps(self, table):
+        values = (f'{steps:.2f}' for steps in self.calibration.config['steps'].values())
+        table.add_row('Current steps', *values, end_section=True)
+
+    def add_measurements(self, table):
+        values = (
+            f'{measurement:.2f}' for measurement in self.calibration.measurements.values()
+        )
+        table.add_row('Measurements', *values)
+
+    def add_errors(self, table):
+        valid_error = self.calibration.config['calcube']['valid-error']
+        values = (
+            f'{utils.format_value(error, threshold=valid_error)}'
+            for error in self.calibration.errors.values()
+        )
+        table.add_row('Errors', *values, end_section=True)
+
+    def add_fixed_steps(self, table):
+        values = []
+        for axis, steps in self.calibration.steps.items():
+            if self.calibration.valid_errors[axis]:
+                display = f'{steps:.2f}'
+            else:
+                display = f'[yellow]{steps:.2f}[/yellow]'
+            values.append(display)
+        table.add_row('Fixed steps', *values)
+
+    def print_table(self, table):
+        self.console.print(table)
+
+    def print_details(self):
+        self.console.print(
+            'Calibrating 3D printer stepping motors from a '
+            f'{self.calibration.config["calcube"]["side"]}mm cube',
+            style='dim italic',
+        )
+
+    def print_gcode(self):
+        fixed_steps = ' '.join(
+            f'{axis}{step:.2f}'
+            for axis, step in self.calibration.steps.items()
+            if not self.calibration.valid_errors[axis]
+        )
+        self.console.print(f'[dark_orange]G-code: M92 {fixed_steps}[/dark_orange]')
+
+    def print(self):
+        table = Table()
+        self.add_header(table)
+        self.add_current_steps(table)
+        self.add_measurements(table)
+        self.add_errors(table)
+        self.add_fixed_steps(table)
+        self.print_table(table)
+        self.print_details()
+        if self.show_gcode:
+            self.print_gcode()
